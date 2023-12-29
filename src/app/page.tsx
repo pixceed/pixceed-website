320ed42337084f722e54f6abb406171191b888c2
pixceed <pixceed@gmail.com>
2023-12-29 17:18:18 +0900

    ヘッダー作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,39 @@
-import Image from 'next/image'
+import { TfiMenu } from "react-icons/tfi";
+import Link from 'next/link'
+import { DotGothic16 } from "next/font/google";
+
+const DotFont =  DotGothic16({
+  weight: "400",
+  subsets: ["latin"],
+});
+
 
 export default function Home() {
   return (
-    <div>
-      hello
-    </div>
+    <>
+      <header className='text-gray-700 border-b-2 border-gray-700'>
+        <div className='container flex mx-auto p-2'>
+          <Link href="/">
+            <div className="text-xl ml-3 font-bold">
+              <span className={DotFont.className} >PIXCEED</span>
+            </div>
+          </Link>
+  
+          <nav className='flex flex-col items-center ml-auto'>
+            <div className="m-auto">
+            <Link className= "mr-4" href="/">
+              <span className={DotFont.className}>HOME</span>
+            </Link>
+            <Link className="mr-4" href="/">
+              <span className={DotFont.className}>MEMO</span>
+            </Link>
+            <Link className="mr-4" href="/">
+              <span className={DotFont.className}>GALLERY</span>
+            </Link>
+            </div>
+          </nav>
+        </div>
+      </header>
+    </>
   )
 }
